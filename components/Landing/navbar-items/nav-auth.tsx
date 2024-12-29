d0e4c00e9f172b5f0afe220ffa39c4e70c0eb205
devansh <devanshverma193@gmail.com>
2024-12-29 19:06:24 +0530

    ui-fix

--- components/Landing/navbar-items/nav-auth.tsx
+++ components/Landing/navbar-items/nav-auth.tsx
@@ -1,18 +1,17 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export const NavAuth = () => {
   return (
-    <>
-      <div className="flex flex-col ml-8 space-x-8 md:flex-row items-center justify-center">
-        <Link
-          href={"/sign-in"}
-          className="font-sans font-medium text-lg hover:bg-white p-2 rounded-lg"
-        >
-          Login
-        </Link>
-        <Button className="font-sans font-medium text-md">Sign up free</Button>
-      </div>
-    </>
+    <div className="flex flex-col sm:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-8 md:ml-8">
+      <Link
+        href="/sign-in"
+        className="font-sans font-medium text-white text-lg hover:bg-neutral-900 hover:text-white transition-colors duration-300 p-2 rounded-lg"
+      >
+        Login
+      </Link>
+      <button className="bg-gradient-to-r from-[#E6D6C8] to-white text-black font-semibold font-sans rounded-xl px-6 py-2 shadow-md hover:shadow-lg transition-transform duration-300 transform hover:scale-105">
+        Get Started for Free
+      </button>
+    </div>
   );
 };
